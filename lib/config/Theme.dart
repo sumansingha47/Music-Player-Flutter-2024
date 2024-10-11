18173065f8dec51fb102f457e123d18e4120129c
SUMAN SINGHA <sumansingha047@gmail.com>
2024-10-11 10:47:09 +0530

    dummy <TrendingSongSlider>, <SongTile> widgets  and SongPage is comlpleted

---
+++ lib/config/Theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:music_player_flutter_2024/config/Colors.dart';
+
+ThemeData darkTheme = ThemeData(
+  useMaterial3: true,
+  brightness: Brightness.dark,
+  colorScheme: const ColorScheme.dark(
+    background: bgColor,
+    primary: primaryColor,
+  ),
+  textTheme: const TextTheme(
+    bodyLarge: TextStyle(
+      fontFamily: "Poppins",
+      fontSize: 18,
+      fontWeight: FontWeight.w500,
+      color: fontColor,
+    ),
+    bodyMedium: TextStyle(
+      fontFamily: "Poppins",
+      fontSize: 18,
+      fontWeight: FontWeight.w400,
+      color: fontColor,
+    ),
+    bodySmall: TextStyle(
+      fontFamily: "Poppins",
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      color: lableColor,
+    ),
+    labelSmall: TextStyle(
+      fontFamily: "Poppins",
+      fontSize: 10,
+      fontWeight: FontWeight.w400,
+      color: lableColor,
+    ),
+    labelMedium: TextStyle(
+      fontFamily: "Poppins",
+      fontSize: 10,
+      fontWeight: FontWeight.w500,
+      color: lableColor,
+    ),
+  ),
+);
